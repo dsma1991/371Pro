379d90d20e8788dcc241022773856313d49a27bc
leekelly12 <lee.kelly12@gmail.com>
2014-11-19 19:10:00 -0500

    Skybox and texture loader
    Includes:
    Skybox in main
    Texture loader
    To be Editted:
    Skybox Image

--- C371GroupProject/C371GroupProject/main.cpp
+++ C371GroupProject/C371GroupProject/main.cpp
@@ -1,4 +1,5 @@
 #include "Model.h"
+#include "imageloader.h"
 
 typedef struct {
 	int width;
@@ -8,7 +9,9 @@ typedef struct {
 	float field_of_view_angle;
 	float z_near;
 	float z_far;
-} glutWindow;
+}
+
+glutWindow;
 
 glutWindow win;
 
@@ -20,6 +23,24 @@ double distance = 150;
 double vertAngle = 3*PI/4, horizAngle = PI/2;
 
 GLUquadricObj * quadratic;
+GLuint _skyboxTexture1;
+float skyboxSize = 4000.0f;
+
+//Load image into texture
+GLuint loadTexture(Image* image){
+		 GLuint textureID;
+	 glGenTextures(1, &textureID); // make room for texture
+	 glBindTexture(GL_TEXTURE_2D, textureID); //tell opengl which texture to edit
+		glTexImage2D(GL_TEXTURE_2D,
+						0,
+						GL_RGB, //format for image
+						image->width, image->height,
+						0,
+						GL_RGB,
+						GL_UNSIGNED_BYTE,
+						image->pixels);
+		return textureID;
+}
 
 // Initiliaze the OpenGL window
 void init(void)
@@ -38,7 +59,96 @@ void init(void)
     glHint( GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST );						// specify implementation-specific hints
 	glClearColor(0.0, 0.0, 0.0, 1.0);
 
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
+	Image* skyboxTexture = loadBMP("starrySky.bmp");
+	_skyboxTexture1 = loadTexture(skyboxTexture);
+	delete(skyboxTexture);
+}
+
+void drawSkybox(){
+	glPushMatrix();
+		glPushAttrib(GL_ENABLE_BIT);
+		glEnable(GL_TEXTURE_2D);
+		glDisable(GL_DEPTH_TEST);
+		glDisable(GL_LIGHTING);
+		glDisable(GL_BLEND);
+
+		//Set all vertices to white
+		glColor4f(1,1,1,1);
+
+		//Render Front Quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glBegin(GL_QUADS);
+			glTexCoord2f(0, 0); glVertex3f(  skyboxSize, -skyboxSize, -skyboxSize );
+			glTexCoord2f(1, 0); glVertex3f( -skyboxSize, -skyboxSize, -skyboxSize );
+			glTexCoord2f(1, 1); glVertex3f( -skyboxSize,  skyboxSize, -skyboxSize );
+			glTexCoord2f(0, 1); glVertex3f(  skyboxSize,  skyboxSize, -skyboxSize );
+		glEnd();
+
+
+		// Render the left quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	   glBegin(GL_QUADS);
+	   		glColor4f(1,1,1,1);
+		   glTexCoord2f(0, 0); glVertex3f(  skyboxSize, -skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 0); glVertex3f(  skyboxSize, -skyboxSize, -skyboxSize );
+		   glTexCoord2f(1, 1); glVertex3f(  skyboxSize,  skyboxSize, -skyboxSize );
+		   glTexCoord2f(0, 1); glVertex3f(  skyboxSize,  skyboxSize,  skyboxSize );
+	   glEnd();
+
+	      // Render the back quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glBegin(GL_QUADS);
+			glTexCoord2f(0, 0); glVertex3f( -skyboxSize, -skyboxSize,  skyboxSize );
+			glTexCoord2f(1, 0); glVertex3f(  skyboxSize, -skyboxSize,  skyboxSize );
+			glTexCoord2f(1, 1); glVertex3f(  skyboxSize,  skyboxSize,  skyboxSize );
+			glTexCoord2f(0, 1); glVertex3f( -skyboxSize,  skyboxSize,  skyboxSize );
+		glEnd();
+
+		// Render the right quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	   glBegin(GL_QUADS);
+		   glTexCoord2f(0, 0); glVertex3f( -skyboxSize, -skyboxSize, -skyboxSize );
+		   glTexCoord2f(1, 0); glVertex3f( -skyboxSize, -skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 1); glVertex3f( -skyboxSize,  skyboxSize,  skyboxSize );
+		   glTexCoord2f(0, 1); glVertex3f( -skyboxSize,  skyboxSize, -skyboxSize );
+	   glEnd();
+
+	   // Render the top quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	   glBegin(GL_QUADS);
+		   glTexCoord2f(0, 1); glVertex3f( -skyboxSize,  skyboxSize, -skyboxSize );
+		   glTexCoord2f(0, 0); glVertex3f( -skyboxSize,  skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 0); glVertex3f(  skyboxSize,  skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 1); glVertex3f(  skyboxSize,  skyboxSize, -skyboxSize );
+	   glEnd();
+
+	   // Render the bottom quad
+		glBindTexture(GL_TEXTURE_2D, _skyboxTexture1); //Texture 1
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	   glBegin(GL_QUADS);
+		   glTexCoord2f(0, 0); glVertex3f( -skyboxSize, -skyboxSize, -skyboxSize );
+		   glTexCoord2f(0, 1); glVertex3f( -skyboxSize, -skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 1); glVertex3f(  skyboxSize, -skyboxSize,  skyboxSize );
+		   glTexCoord2f(1, 0); glVertex3f(  skyboxSize, -skyboxSize, -skyboxSize );
+	   glEnd();
+	
+	   // Restore enable bits and matrix
+		glPopAttrib();
+	glPopMatrix();
 }
 
 void display(void)
@@ -49,8 +159,14 @@ void display(void)
 	gluLookAt(eyeX,eyeY,eyeZ,centerX,centerY,centerZ,0,1,0);
 	//gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0); //using this to test my stuff -Arteum
 
-	glutSolidCube(20);
+	glPushMatrix();
+//	glColor3f(1.0f, 0.0f, 0.0f);
+//		glutSolidCube(20);
+		drawSkybox();
+
 	glPopMatrix();
+	
+
 	glFlush();
 	glutSwapBuffers();
 }
@@ -145,10 +261,10 @@ int main(int argc, char** argv)
 	// set window values
 	win.width = 640;
 	win.height = 480;
-	win.title = "Da project is here!!!11!!!one!!!1!!";
+	win.title = "Robot Siege";
 	win.field_of_view_angle = 45;
 	win.z_near = 1.0f;
-	win.z_far = 500.0f;
+	win.z_far = 9000.0f;
 
 	
 
